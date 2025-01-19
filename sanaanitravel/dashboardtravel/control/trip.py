dc71e3d9d89873ec0b089f2878489fbb1c332a2c
Emad <emadalhammadi964@outlook.com>
2025-01-19 23:12:16 +0300

    Add trip management functionality with listing, detail, addition, editing, and deletion of trips

---
+++ sanaanitravel/dashboardtravel/control/trip.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from ..models import Trip,City,TravelType,TripCategory,Vehicle
+from django.db.models import Q
+import os
+from django.contrib.auth.decorators import login_required
+#####################################  ادارة الرحلات ##################################################################
+
+@login_required(login_url='login')
+def trip_list(request):
+    query = request.GET.get('q', '')
+    trips = Trip.objects.filter(Q(departure__name__icontains=query) | Q(destination__name__icontains=query))
+    city = City.objects.all()  
+    traveltype = TravelType.objects.all()  
+    tripcategory = TripCategory.objects.all()  
+    vehicle = Vehicle.objects.all()
+    return render(request, 'dashboard/Trips.html', {'trips': trips, 'query': query,'city': city,'traveltype': traveltype,'tripcategory': tripcategory,'vehicle': vehicle})
+
+
+@login_required(login_url='login')
+def trip_detail(request, trip_id):
+    trip = Trip.objects.get(id=trip_id)
+    return render(request, 'dashboard/Trips_detail.html', {'trip': trip})
+
+
+
+@login_required(login_url='login')
+def add_trip(request):
+    if request.method == 'POST':
+        city_id_from= request.POST.get('departure') 
+        city_id_to= request.POST.get('destination') 
+        travel_type_id= request.POST.get('travel_type') 
+        trip_category_id= request.POST.get('trip_category') 
+        vehicle_type_id= request.POST.get('vehicle_type') 
+
+        departure = get_object_or_404(City, id=city_id_from) 
+        destination = get_object_or_404(City, id=city_id_to) 
+        travel_type = get_object_or_404(TravelType, id=travel_type_id) 
+        trip_category = get_object_or_404(TripCategory, id=trip_category_id) 
+        vehicle_type = get_object_or_404(Vehicle, id=vehicle_type_id) 
+
+        trip = Trip(
+            departure=departure,
+            destination=destination,
+            travel_type=travel_type,
+            trip_category=trip_category,
+            vehicle_type=vehicle_type,
+            date=request.POST['date'],
+            time=request.POST['time'],
+            seat_count=request.POST['seat_count'],
+            seat_price=request.POST['seat_price'],
+            details=request.POST['details'],
+            image=request.FILES['image']
+        )
+        trip.save()
+        return redirect('trip_list')
+    return render(request, 'dashboard/Trips.html')
+
+
+@login_required(login_url='login')
+def edit_trip(request):
+    if request.method == 'POST':
+        trip_id = request.POST.get('id')
+        trip = get_object_or_404(Trip, id=trip_id)
+        city_id_from= request.POST.get('departure') 
+        city_id_to= request.POST.get('destination') 
+        travel_type_id= request.POST.get('travel_type') 
+        trip_category_id= request.POST.get('trip_category') 
+        vehicle_type_id= request.POST.get('vehicle_type') 
+
+        departure = get_object_or_404(City, id=city_id_from) 
+        destination = get_object_or_404(City, id=city_id_to) 
+        travel_type = get_object_or_404(TravelType, id=travel_type_id) 
+        trip_category = get_object_or_404(TripCategory, id=trip_category_id) 
+        vehicle_type = get_object_or_404(Vehicle, id=vehicle_type_id) 
+
+        trip.departure = departure
+        trip.destination = destination
+        trip.travel_type = travel_type
+        trip.trip_category = trip_category
+        trip.vehicle_type = vehicle_type
+        trip.date = request.POST['date']
+        trip.time = request.POST['time']
+        trip.seat_count = request.POST['seat_count']
+        trip.seat_price = request.POST['seat_price']
+        trip.details = request.POST['details']
+        
+        if 'image' in request.FILES and request.FILES['image']:
+            trip.image = request.FILES['image']
+        
+        trip.save() 
+        return redirect('trip_list')
+
+
+
+    return render(request, 'dashboard/Trips.html', {'trip': trip})
+
+
+@login_required(login_url='login')
+def delete_trip(request):
+    if request.method == 'POST':
+        trip_id = request.POST.get('id')
+        trip = get_object_or_404(Trip, id=trip_id)
+
+        if trip.image:
+            if os.path.isfile(trip.image.path):
+                os.remove(trip.image.path)
+        trip.delete() 
+        return redirect('trip_list') 
+    return redirect('trip_list')
+
+
+#####################################  ادارة الرحلات ##################################################################
