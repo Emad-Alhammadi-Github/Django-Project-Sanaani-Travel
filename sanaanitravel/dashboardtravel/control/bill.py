4c3dcbb7f45c236791b149a1d04916171c296c03
Emad <emadalhammadi964@outlook.com>
2025-01-19 23:10:32 +0300

    Add invoice management functionality with list, add, edit, and delete features

---
+++ sanaanitravel/dashboardtravel/control/bill.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from ..models import Trip, Passenger, Invoice,ReservationRequest
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch, Q
+#####################################  ادارة الفواتير ##################################################################
+
+# @login_required(login_url='login')
+# def invoice_list(request):
+#     query = request.GET.get('q', '')
+#     invoices = Invoice.objects.filter(Q(passenger__name__icontains=query))
+#     trips = Trip.objects.all() 
+#     return render(request, 'dashboard/Bills.html', {'invoices': invoices,'trips': trips, 'query': query})
+
+
+
+@login_required(login_url='login')
+def invoice_list(request):
+    query = request.GET.get('q', '')
+
+    invoices = Invoice.objects.filter(
+        Q(passenger__name__icontains=query)
+    ).prefetch_related(
+        Prefetch(
+            'passenger__reservation_requests',
+            queryset=ReservationRequest.objects.filter(status='approved'),
+            to_attr='approved_reservations'
+        )
+    )
+
+    trips = Trip.objects.all()
+
+    for invoice in invoices:
+        if invoice.passenger.approved_reservations:
+            invoice.reservation_date = invoice.passenger.approved_reservations[0].request_date
+        else:
+            invoice.reservation_date = None
+    return render(request, 'dashboard/Bills.html', {
+        'invoices': invoices,
+        'trips': trips,
+        'query': query,
+    })
+
+
+
+
+@login_required(login_url='login')
+def add_invoice(request):
+    if request.method == 'POST':
+        invoice = Invoice(
+            passenger=Passenger.objects.get(id=request.POST['passenger']),
+            paid_amount=request.POST['paid_amount'],
+            remaining_amount=request.POST['remaining_amount'],
+            trip=Trip.objects.get(id=request.POST['trip']),
+            seat_number=request.POST['seat_number'],
+            trip_date=request.POST['trip_date'],
+            total_amount=request.POST['total_amount']
+        )
+        invoice.save()
+        return redirect('invoice_list')
+    passengers = Passenger.objects.all()
+    trips = Trip.objects.all()
+    return render(request, 'trips/add_invoice.html', {'passengers': passengers, 'trips': trips})
+
+
+
+@login_required(login_url='login')
+def edit_invoice(request, invoice_id):
+    invoice = get_object_or_404(Invoice, id=invoice_id)
+    if request.method == 'POST':
+        invoice.passenger = Passenger.objects.get(id=request.POST['passenger'])
+        invoice.paid_amount = request.POST['paid_amount']
+        invoice.remaining_amount = request.POST['remaining_amount']
+        invoice.trip = Trip.objects.get(id=request.POST['trip'])
+        invoice.seat_number = request.POST['seat_number']
+        invoice.trip_date = request.POST['trip_date']
+        invoice.total_amount = request.POST['total_amount']
+        invoice.save()
+        return redirect('invoice_list')
+    passengers = Passenger.objects.all()
+    trips = Trip.objects.all()
+    return render(request, 'trips/edit_invoice.html', {'invoice': invoice, 'passengers': passengers, 'trips': trips})
+
+
+
+@login_required(login_url='login')
+def delete_invoice(request, invoice_id):
+    invoice = get_object_or_404(Invoice, id=invoice_id)
+    if request.method == 'POST':
+        invoice.delete()
+        return redirect('invoice_list')
+    return render(request, 'trips/delete_invoice.html', {'invoice': invoice})
+#####################################  ادارة الفواتير ##################################################################
