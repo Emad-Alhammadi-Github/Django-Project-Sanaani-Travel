be7d6c349a98a1de370993fc0a9bd27adb403f39
Emad <emadalhammadi964@outlook.com>
2025-02-07 18:01:57 +0300

    Refactor reservation management to require admin login and streamline reservation approval/rejection processes

--- sanaanitravel/dashboardtravel/control/reservation.py
+++ sanaanitravel/dashboardtravel/control/reservation.py
@@ -4,98 +4,65 @@ from django.http import HttpResponseRedirect,JsonResponse
 from django.contrib.auth.decorators import login_required
 
 
-@login_required(login_url='login')
+@login_required(login_url='loginadmin')
 def reservation_requests(request):
     reservations = ReservationRequest.objects.filter(status='pending')
     return render(request, 'dashboard/reservation_requests.html', {'reservations': reservations})
 
+@login_required(login_url='loginadmin')
+def approve_reservation_list(request):
+    reservations_approved = ReservationRequest.objects.filter(status='approved')
+    return render(request, 'dashboard/approve_reservation.html', {'reservations_approved': reservations_approved})
 
 
-
-# @login_required(login_url='login')
-# def approve_reservation(request, reservation_id):
-#     reservation = get_object_or_404(ReservationRequest, id=reservation_id)
-
-#     # تغيير حالة الحجز إلى 'approved'
-#     reservation.status = 'approved'
-#     reservation.save()
-
-#     # إضافة المسافر إلى جدول Passenger
-#     passenger = Passenger(
-#         name=reservation.passenger.name,
-#         id_number=reservation.passenger.id_number,
-#         passport_number=reservation.passenger.passport_number,
-#         phone=reservation.passenger.phone,
-#         trip_location=reservation.passenger.trip_location,
-#         paid_amount=reservation.passenger.paid_amount,
-#         remaining_amount=reservation.passenger.remaining_amount,
-#         seat_number=reservation.passenger.seat_number,
-#         gender=reservation.passenger.gender,
-#         nationality=reservation.passenger.nationality,
-#         image=reservation.passenger.image
-#     )
-#     passenger.save()
-
-#     # إنشاء الفاتورة للمسافر
-#     vtt = reservation.passenger.trip_location.vehicle_type
-#     invoice = Invoice.objects.create(
-#         passenger=passenger,
-#         type_vehicle=vtt,
-#         paid_amount=passenger.paid_amount,
-#         remaining_amount=passenger.remaining_amount,
-#         trip=passenger.trip_location,
-#         seat_number=passenger.seat_number,
-#         trip_date=passenger.trip_location.date,
-#         time=passenger.trip_location.time,
-#         total_amount=passenger.paid_amount + passenger.remaining_amount,
-#     )
-#     invoice.save()
-
-#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))  # العودة إلى الصفحة السابقة
+@login_required(login_url='loginadmin')
+def reject_reservation_list(request):
+    reservations_rejected = ReservationRequest.objects.filter(status='rejected')
+    return render(request, 'dashboard/reject_reservation.html', {'reservations_rejected': reservations_rejected})
 
 
-
-
-
-
-
-@login_required(login_url='login')
+@login_required(login_url='loginadmin')
 def approve_reservation(request, reservation_id):
     reservation = get_object_or_404(ReservationRequest, id=reservation_id)
 
     reservation.status = 'approved'
     reservation.save()
-    # passenger = reservation.passenger
-    # passenger.save()
-    
-    # الحصول على المسافر المرتبط بالحجز
+
     passenger = reservation.passenger
     vtt=reservation.passenger.trip_location.vehicle_type
 
-    # إنشاء الفاتورة
     invoice = Invoice.objects.create(
         passenger=passenger,
-        type_vehicle=vtt,  # افترض أن الرحلة تحتوي على مركبة
+        type_vehicle=vtt, 
         paid_amount=passenger.paid_amount,
         remaining_amount=passenger.remaining_amount,
         trip=passenger.trip_location,
         seat_number=passenger.seat_number,
         trip_date=passenger.trip_date,
-        time=passenger.trip_location.time,  # افترض أن الرحلة تحتوي على وقت
-        total_amount=passenger.paid_amount + passenger.remaining_amount,  # حساب الإجمالي
+        time=passenger.trip_location.time,  
+        total_amount=passenger.paid_amount + passenger.remaining_amount,  
     )
 
-    # حفظ الفاتورة
     invoice.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-@login_required(login_url='login')
+@login_required(login_url='loginadmin')
 def reject_reservation(request, reservation_id):
     reservation = get_object_or_404(ReservationRequest, id=reservation_id)
     reservation.status = 'rejected'
     reservation.save()
 
+    # passenger_id=  reservation.passenger.id
+    # passenger = get_object_or_404(Passenger, id=passenger_id)
+    # passenger.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required(login_url='loginadmin')
+def reject_reservation_delete(request, reservation_id):
+    reservation = get_object_or_404(ReservationRequest, id=reservation_id)
+    reservation.delete()
     passenger_id=  reservation.passenger.id
     passenger = get_object_or_404(Passenger, id=passenger_id)
     passenger.delete()
