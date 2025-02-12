8356058afcaa7a4055fb8a951e1cf5b04d0c3660
Emad <emadalhammadi964@outlook.com>
2025-02-13 00:36:18 +0300

    Add check for Yemeni trip nationality in checkout process

--- sanaanitravel/dashboardtravel/control/checkOut.py
+++ sanaanitravel/dashboardtravel/control/checkOut.py
@@ -10,6 +10,7 @@ from datetime import datetime
 def checkout(request, trip_id):
     trip = get_object_or_404(Trip, id=trip_id)
     nationalities = Nationality.objects.all() 
+    is_yemeni_trip = trip.departure.nationality.name == "اليمن" and trip.destination.nationality.name == "اليمن"
     if request.method == 'POST':
         nationality_id = request.POST.get('nationality') 
         nationality = get_object_or_404(Nationality, id=nationality_id)
@@ -18,7 +19,6 @@ def checkout(request, trip_id):
         passport_number = request.POST.get('passport_number')
         phone = request.POST.get('phone')
         paid_amount = request.POST.get('paid_amount')
-        trip_date = request.POST.get('trip_date')
         gender = request.POST.get('gender')
         image = request.FILES.get('image')
         date_of_birth=request.POST.get('date_of_birth')
@@ -46,7 +46,6 @@ def checkout(request, trip_id):
             trip_location_id=trip.id,
             paid_amount=paid_amount_decimal,
             remaining_amount=remaining_amount,
-            trip_date=trip.date,
             seat_number=seat_number,
             gender=gender,
             nationality=nationality,
@@ -64,65 +63,5 @@ def checkout(request, trip_id):
         return redirect('success_page')
 
     
-    return render(request, 'home/checkOut.html', {'trip': trip,'nationalities': nationalities})
+    return render(request, 'home/checkOut.html', {'trip': trip,'is_yemeni_trip': is_yemeni_trip,'nationalities': nationalities})
 
-
-
-
-
-# def checkout(request, trip_id):
-#     trip = get_object_or_404(Trip, id=trip_id)
-#     nationalities = Nationality.objects.all() 
-#     if request.method == 'POST':
-#         nationality_id = request.POST.get('nationality') 
-#         nationality = get_object_or_404(Nationality, id=nationality_id)
-#         name = request.POST.get('name')
-#         id_number = request.POST.get('id_number')
-#         passport_number = request.POST.get('passport_number')
-#         phone = request.POST.get('phone')
-#         paid_amount = request.POST.get('paid_amount')
-#         gender = request.POST.get('gender')
-#         image = request.FILES.get('image')
-
-#         # التحقق من وجود مقاعد شاغرة
-#         passengers_count = Passenger.objects.filter(trip_location=trip).count()
-#         if passengers_count >= trip.seat_count:
-#             return JsonResponse({'error': 'No seats available for this trip.'}, status=400)
-
-#         seat_number = passengers_count + 1
-
-#         # حساب المبلغ المدفوع والمبلغ المتبقي
-#         seat_price = trip.seat_price
-#         paid_amount_decimal = Decimal(paid_amount) if paid_amount else Decimal(0)
-#         remaining_amount = seat_price - paid_amount_decimal  
-
-#         # حفظ الحجز في جدول ReservationRequest مع حالة 'pending'
-#         reservation_request = ReservationRequest(
-#             passenger=None,  # سيتم تعيين المسافر بعد الموافقة
-#             request_date=datetime.now().date(),
-#             status='pending'
-#         )
-#         reservation_request.save()
-
-#         # حفظ بيانات الحجز في جدول المسافر بعد الموافقة
-#         passenger = Passenger(
-#             name=name,
-#             id_number=id_number,
-#             passport_number=passport_number,
-#             phone=phone,
-#             trip_location=trip,
-#             paid_amount=paid_amount_decimal,
-#             remaining_amount=remaining_amount,
-#             seat_number=seat_number,
-#             gender=gender,
-#             nationality=nationality,
-#             image=image
-#         )
-#         passenger.save()
-
-#         reservation_request.passenger = passenger
-#         reservation_request.save()
-
-#         return redirect('reservation_requests')  
-
-#     return render(request, 'home/checkOut.html', {'trip': trip, 'nationalities': nationalities})
