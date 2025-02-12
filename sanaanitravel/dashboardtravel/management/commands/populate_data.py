3cf2c3038856b10e3ffba37f01a43b36c82cdeaa
Emad <emadalhammadi964@outlook.com>
2025-02-13 00:37:02 +0300

    Add initial data population for employees and users in populate_data command

--- sanaanitravel/dashboardtravel/management/commands/populate_data.py
+++ sanaanitravel/dashboardtravel/management/commands/populate_data.py
@@ -1,6 +1,12 @@
 from django.core.management.base import BaseCommand
-from dashboardtravel.models import Nationality, City, TravelType, TripCategory,Employee
+from dashboardtravel.models import Nationality, City, TravelType, TripCategory,Employee,Driver,Vehicle,Trip
 from django.contrib.auth.models import User
+import requests
+from faker import Faker
+from random import choice, randint
+from django.core.files.base import ContentFile
+from django.core.files import File
+from io import BytesIO
 class Command(BaseCommand):
     help = 'Populate database with initial data'
 
@@ -56,7 +62,6 @@ class Command(BaseCommand):
             ]
         }
 
-        # إضافة المدن لكل دولة
         for country, cities in countries_and_cities.items():
             nationality = Nationality.objects.get(name=country)
             for city in cities:
@@ -88,7 +93,7 @@ class Command(BaseCommand):
             TravelType.objects.get_or_create(name=travel_type)
 
         trip_categories = [
-        'خاصة', 'عامة','الداخلية','الخارجية'
+        'خاصة', 'عامة',
         ]
 
         for trip_category in trip_categories:
@@ -119,5 +124,154 @@ class Command(BaseCommand):
             user_type='admin',
             nationality=yemen_nationality,
         )
+        employee_user = User.objects.create(
+            username='emp',
+            email='emp@gmail.com', 
+            password='emp123'
+        )
+
+        admin_user.is_active = True
+        admin_user.save()
+
+        Employee.objects.create(
+            user=employee_user,
+            name='Employee', 
+            job_title='موظف', 
+            phone='12345',
+            id_number='123450',
+            salary=10000,
+            gender='1',
+            user_type='employee',
+            nationality=yemen_nationality,
+        )
+
+        manmonay_user = User.objects.create(
+            username='manmon',
+            email='manmon@gmail.com', 
+            password='manmon123'
+        )
+
+        admin_user.is_active = True
+        admin_user.save()
+
+        Employee.objects.create(
+            user=manmonay_user,
+            name='Manmonay', 
+            job_title='مسوول مالي', 
+            phone='1234567',
+            id_number='12345670',
+            salary=10000,
+            gender='1',
+            user_type='manmonay',
+            nationality=yemen_nationality,
+        )
+
+
+    #     fake = Faker()
+    #     driver_images = [self.fetch_image() for _ in range(10)]
+    #     vehicle_images = [self.fetch_image_vehicle() for _ in range(10)]
+    #     trip_images = [self.fetch_imagetrip() for _ in range(10)]
+
+    #    
+    #     for i in range(10):
+    #         nationality = Nationality.objects.order_by('?').first()
+    #         driver = Driver.objects.create(
+    #             name=fake.name(),
+    #             experience_years=randint(1, 20),
+    #             phone=fake.phone_number(),
+    #             license_type=choice(['12 راكب', '6 ركاب','16 راكب',]),
+    #             license_number=fake.license_plate(),
+    #             license_img=driver_images[i],
+    #             id_number=fake.ssn(),
+    #             identify_img=driver_images[i],
+    #             passport_number=fake.passport_number(),
+    #             gender=choice(['1', '0']),
+    #             nationality=nationality,
+    #             image=driver_images[i],
+    #             date_of_birth=fake.date_of_birth(minimum_age=20, maximum_age=60),
+    #         )
+
+    #  
+    #     for i in range(10):
+    #         driver = Driver.objects.order_by('?').first()
+    #         Vehicle.objects.create(
+    #             name=fake.company() + " " + fake.word(),  # تم تعديل هنا
+    #             vehicle_type=choice(['حافلة', 'باص رويشان', 'هيلوكس', 'رافور', 'كورلا', 'ليكزاز', 'مرسيدس', 'بي ام دبليو']),
+    #             model=fake.word(),
+    #             plate_number=fake.license_plate(),
+    #             status=choice(['new', 'old', 'in_service']),
+    #             price=randint(10000, 50000),
+    #             owner=fake.name(),
+    #             passenger_capacity=randint(1, 50),
+    #             fuel_capacity=randint(30, 100),
+    #             motor_type=choice(['بترول', 'ديزل', 'كهرباء','غاز']),
+    #             driver=driver,
+    #             description=fake.text(),
+    #             image=vehicle_images[i],
+    #             img1=vehicle_images[i],
+    #         )
+
+    #     
+    #     for i in range(10):
+    #         departure_city = City.objects.order_by('?').first()
+    #         destination_city = City.objects.exclude(id=departure_city.id).order_by('?').first()
+    #         vehicle = Vehicle.objects.order_by('?').first()
+    #         travel_type = TravelType.objects.order_by('?').first()
+    #         trip_category = TripCategory.objects.order_by('?').first()
+
+    #         Trip.objects.create(
+    #             departure=departure_city,
+    #             destination=destination_city,
+    #             travel_type=travel_type,
+    #             trip_category=trip_category,
+    #             vehicle_type=vehicle,
+    #             date=fake.date(),
+    #             time=fake.time(),
+    #             seat_count=randint(1, 50),
+    #             seat_price=randint(10, 100),
+    #             details=fake.text(),
+    #             image=trip_images[i],
+    #         )
+
+    #     self.stdout.write(self.style.SUCCESS('Data populated successfully.'))
+
+    # def fetch_image(self):
+    #     image_urls = [
+    #         'https://www.pexels.com/photo/photography-of-a-guy-wearing-green-shirt-1222271.jpeg',  
+    #     ]
+    #     url = choice(image_urls)
+    #     response = requests.get(url)
+    #     if response.status_code == 200:
+    #         return ContentFile(response.content, name=f'driver_image_{randint(1, 10000)}.jpg') 
+    #     else:
+    #         return ContentFile(response.content, name=f'driver_image_{randint(1, 10000)}.jpg') 
+    # def fetch_image_vehicle(self):
+    #     image_urls = [
+    #     'https://images.pexels.com/photos/2036544/pexels-photo-2036544.jpeg',  
+    #     ]
+    #     url = choice(image_urls)
+    #     response = requests.get(url)
+    #     if response.status_code == 200:
+    #         return ContentFile(response.content, name=f'vehicle_image_{randint(1, 10000)}.jpg')
+    #     else:
+    #         return ContentFile(response.content, name=f'vehicle_image_{randint(1, 10000)}.jpg')    
+
+    # def fetch_imagetrip(self):
+    #     image_urls = [
+    #     'https://images.pexels.com/photos/30459688/pexels-photo-30459688.jpeg',  
+    #     ]
+    #     url = choice(image_urls)
+    #     response = requests.get(url)
+    #     if response.status_code == 200:
+    #         return ContentFile(response.content, name=f'trip_image_{randint(1, 10000)}.jpg')
+    #     else:
+    #         return ContentFile(response.content, name=f'trip_image_{randint(1, 10000)}.jpg')
+
+
+
+
+
+
+
 
         self.stdout.write(self.style.SUCCESS('Data populated successfully.'))
