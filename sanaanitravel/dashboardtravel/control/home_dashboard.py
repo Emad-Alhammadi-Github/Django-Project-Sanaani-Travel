8d36a0fa129fd5d2151fbdcbbcc78f986c395fc7
Emad <emadalhammadi964@outlook.com>
2025-02-13 00:36:23 +0300

    Update income calculations to use date_added instead of trip_date

--- sanaanitravel/dashboardtravel/control/home_dashboard.py
+++ sanaanitravel/dashboardtravel/control/home_dashboard.py
@@ -14,10 +14,10 @@ from django.contrib.auth.decorators import login_required
 
 def home_dashboard(request):
     today = date.today()
-    today_income = Passenger.objects.filter(trip_date=today).aggregate(total_income=Sum('paid_amount'))['total_income'] or 0
+    today_income = Passenger.objects.filter(date_added=today).aggregate(total_income=Sum('paid_amount'))['total_income'] or 0
     print(f"Income for today: {today_income}")
     current_year = date.today().year
-    year_income = Passenger.objects.filter(trip_date__year=current_year).aggregate(total_income=Sum('paid_amount'))['total_income'] or 0
+    year_income = Passenger.objects.filter(date_added__year=current_year).aggregate(total_income=Sum('paid_amount'))['total_income'] or 0
     print(f"Income for the year: {year_income}") 
     
     return render(request,'base.html', {
